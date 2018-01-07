ebe6d1c90707a518aab1dacc6d789efc552a6b56
Jitendra <jitendra@jitendra-UNLOCK-INSTALL>
2018-01-07 22:01:53 +0100

    qcov and identify added

--- Polyploidism.pl
+++ Polyploidism.pl
@@ -30,7 +30,7 @@ Keep an eye at following line:
 $ENV{'PATH'} = "/bin:/usr/bin:/usr/bin/env:$ENV{PWD}/augustus.2.5.5/bin: $ENV{PWD}/augustus.2.5.5/config: $ENV{PWD}/lastz-distrib-1.03.73/src: $ENV{PWD}/KaKs_Calculator2.0/bin/Linux: $ENV{PWD}/muscle: $ENV{PWD}/dotter: $ENV{PWD}/trf";
 
 
-FOR GENE try: perl Polyploidism.pl -f xaa.fa -o TESTOUT3 -s elegans -e 4 -g -i 90 -t 7 -z 5
+FOR GENE try: perl Polyploidism.pl -f xaa.fa -o TESTOUT3 -s elegans -q 50 -e 4 -g -i 70 -t 7 -z 5
 FOR RANDOM: perl Polyploidism.pl -f sampleSeq.fa -o TESTOUT3 -s elegans -q 50 -e 4 -r -i 70 -t 7 -l 500 -c 2 -z 5
 
 -------------------------------------------------------------------
